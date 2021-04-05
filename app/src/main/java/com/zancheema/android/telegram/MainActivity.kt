68e243cd79943d5aecdfe264b4f5e142c0c97e21
= <social.zaincheema@gmail.com>
2021-04-05 12:29:53 +0500

    desinged main sidebar

--- app/src/main/java/com/zancheema/android/telegram/MainActivity.kt
+++ app/src/main/java/com/zancheema/android/telegram/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.zancheema.android.telegram
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +16,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbarMain)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.mainDrawerLayout)
+        val navView = findViewById<NavigationView>(R.id.navView)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
